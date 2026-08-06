c96526fd3fa4b3a80abcfb419ecef39820b37073
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-06 11:16:25 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(a);
+        System.out.println(a += 10);
+        System.out.println(a -= 10);
+    }
+}
