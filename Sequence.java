08107e97cf8106dae5b958748045536e51394114
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-06 11:42:41 +0530

    Add files via upload

---
+++ Sequence.java
@@ -0,0 +1,10 @@
+public class Sequence {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = a + b;
+
+        System.out.println(c);
+    }
+}
