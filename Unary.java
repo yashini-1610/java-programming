d9313ce8ca55e0c3da2a56fb3eb7777a197bd71b
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-06 11:22:25 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,10 @@
+public class Unary {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+    }
+}
