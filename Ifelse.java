894ecb977cf4baef0743dfeb0e5d4e64d59acd23
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-18 15:20:57 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse{
+    public static void main(String[] args) {
+        byte age = 12;
+
+        if (age >= 18) {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not eligible");
+        }
+    }
+}
