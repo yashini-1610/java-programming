8a8f063db533025f967d16a577ec1724874dda0d
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-18 14:28:01 +0530

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,9 @@
+public class Control {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if (a > 10) {
+            System.out.println("A greater than 10");
+        }
+    }
+}
