970ae5a0eef6b6bf0fb993479f0e1e3a67f7d67d
yashini-1610 <yashinimoorthy9@gmail.com>
2026-08-18 14:32:54 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,11 @@
+public class SimpleIf {
+    public static void main(String[] args) {
+        byte age = 21;
+
+        if (age >= 18) {
+            System.out.println("Eligible");
+        }
+
+        System.out.println("End if");
+    }
+}
