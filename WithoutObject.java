7fcd390745ff36a794b53c409ea3d9a19d89cf1a
yashini-1610 <yashinimoorthy9@gmail.com>
2026-09-22 15:23:25 +0530

    Add files via upload

---
+++ WithoutObject.java
@@ -0,0 +1,13 @@
+public class WithoutObject {
+    static String name = "ABCD";
+    static int age = 15;
+
+    static void display() {
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
